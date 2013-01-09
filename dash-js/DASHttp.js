23d8dfdbacb3a9eace947b742eb0e43159edabc1
Giuseppe Longo <peppe6000@yahoo.it>
2013-01-09 16:29:46 +0100

    Primo commit

---
+++ dash-js/DASHttp.js
@@ -0,0 +1,191 @@
+/*
+ * DASHttp.js
+ *****************************************************************************
+ * Copyright (C) 2012 - 2013 Alpen-Adria-Universität Klagenfurt
+ *
+ * Created on: Feb 13, 2012
+ * Authors: Benjamin Rainer <benjamin.rainer@itec.aau.at>
+ *          Stefan Lederer  <stefan.lederer@itec.aau.at>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+ 
+ var _timeID = 0;
+ var _tmpvideo;
+ var _cacheControl;
+ 
+function DASHttp()
+{
+	
+	
+}
+
+
+// this method is used by the mediaSourceBuffer to push segments in
+function _push_segment_to_media_source_api(buffer, data)
+{
+    console.log("DASH-JS client: appending data of length: " + data.length + " to the Media Source Buffer with id: "+ buffer.id);
+    sourceBufferAppend(dashPlayer.MSE, buffer.id, data);
+   
+}
+
+function _fetch_segment(presentation, url, video, range, buffer)
+{
+	console.log('DASH JS Client fetching segment: ' + url);
+	var xhr = new XMLHttpRequest();
+	xhr.timeID = _timeID;
+	xhr.open('GET', url, true);
+	xhr.setRequestHeader('Cache-Control', _cacheControl);
+
+	if(range != null)
+	{
+		xhr.setRequestHeader('Range', 'bytes='+range);
+		console.log('DASH JS Client fetching byte range: ' + range);
+	}
+    
+	xhr.responseType = 'arraybuffer';
+	
+	//_tmpvideo = video;
+	xhr.onload = function(e)
+   		 {
+
+     			data = new Uint8Array(this.response);
+    			mybps = endBitrateMeasurementByID(this.timeID,data.length);
+    			myBandwidth.calcWeightedBandwidth(parseInt(mybps));
+    			adaptation.switchRepresentation();
+    			    
+			    _push_segment_to_media_source_api(buffer, data);
+    			    
+			    if(extCount==300) video.webkitSourceEndOfStream(HTMLMediaElement.EOS_NO_ERROR);
+        
+   		 };
+	
+	beginBitrateMeasurementByID(this._timeID);
+	_timeID++;
+	xhr.send();
+}
+
+
+function _fetch_segment_for_buffer(presentation, url, video, range, buffer)
+{
+    console.log('DASH JS Client fetching segment: ' + url);
+	var xhr = new XMLHttpRequest();
+	xhr.timeID = _timeID;
+	xhr.open('GET', url, true);
+	xhr.setRequestHeader('Cache-Control', _cacheControl);
+	if(range != null)
+	{
+		xhr.setRequestHeader('Range', 'bytes='+range);
+		console.log('DASH JS Client fetching byte range: ' + range);
+	}
+	
+	xhr.responseType = 'arraybuffer';
+	xhr.buffer = buffer;
+	//_tmpvideo = video;
+	xhr.onload = function(e)
+	{
+		
+		if(xhr.status == 404) {
+		
+			segNotAvailable = 1;
+			requestDate = new Date();
+			
+		} else {
+			
+			countSegmentRequest = 0;
+			
+			data = new Uint8Array(this.response);
+			mybps = endBitrateMeasurementByID(this.timeID,data.length);
+			myBandwidth.calcWeightedBandwidth(parseInt(mybps));
+			
+			adaptation.switchRepresentation();      // <--- mod this, if you wanna change the adaptation behavior ... (e. g., include buffer state, ...)
+			
+			// push the data into our buffer - 2 sono secondi
+			buffer.push(data, 2);
+			
+			if(extCount==300) buffer.streamEnded = true;
+			
+			buffer.callback();
+		}
+		
+	};
+	
+	beginBitrateMeasurementByID(this._timeID);
+	_timeID++;
+	xhr.send();
+	
+}
+
+
+				
+function _dashSourceOpen(buffer, presentation, video, mediaSource)
+{
+	// check the parsed mpd
+	// fetch a representation and check whether selfinitialized or ...
+		
+	console.log('Entering _dashSourceOpen()...');
+	video.width = presentation.width;
+	video.height = presentation.height;
+	
+	extContentType = presentation.mimeType + '; codecs="' + presentation.codecs + '"';
+	console.log("DASJ-JS: content type: " + extContentType);
+	//viene creato un sourceBuffer per il mediaSource
+	addSourceBuffer(mediaSource, buffer.id, extContentType);
+	
+	console.log("extCount = "+extCount);
+	
+	if(presentation.hasInitialSegment == false)
+	{
+		console.log("myMPD.period[0].group[0].segmentTemplate.initialization = "+myMPD.period[0].group[0].segmentTemplate.initialization);
+        baseURL = presentation.baseURL;
+		//qui viene caricato il segmento; la URL è formata da baseUrl+segmento
+		_fetch_segment(presentation, (baseURL != 'undefined' ? presentation.baseURL : '') + myMPD.period[0].group[0].segmentTemplate.initialization, video, null, buffer);
+				
+	}else{
+		baseURL = presentation.baseURL;
+		//qui viene caricato il segmento; la URL è formata da baseUrl+segmento
+		_fetch_segment(presentation, (baseURL != 'undefined' ? presentation.baseURL : '') + adaptation.getInitialChunk(presentation).src, video, adaptation.getInitialChunk(presentation).range, buffer);
+		//presentation.curSegment++;
+	}
+			
+}
+
+function _dashFetchSegmentBuffer(presentation, video, buffer)
+{
+	console.log("extCount = "+extCount);
+	if(extCount==300) {
+        return; 
+    }
+    baseURL = presentation.baseURL;
+	
+	_fetch_segment_for_buffer(presentation, (baseURL != 'undefined' ? presentation.baseURL : '') + myMPD.period[0].group[0].segmentTemplate.media.replace("$Number%03$", extCount), video, null, buffer);
+	
+	extCount++;
+	
+}
+
+
+function _dashFetchSegmentAsynchron(buffer, callback)
+{
+	_dashFetchSegmentBuffer(adaptation.currentRepresentation, adaptation.mediaElement, buffer);
+}
+ 
+ 
+function initDASHttp(cacheControl)
+{
+	_timeID = 0;
+	_cacheControl = cacheControl;
+	
+}
---
+++ dash-js/dash.js
@@ -0,0 +1,126 @@
+var DASHJS_VERSION = "0.5a";
+var dashInstance;
+var playbackTimePlot;
+
+var extCount;
+var myMPD;
+var segNotAvailable;
+var countSegmentRequest;
+var requestDate;
+var extContentType;
+
+function updatePlaybackTime()
+{
+    playbackTimePlot.update(dashInstance.videoTag.currentTime, 2);
+    window.setTimeout(function () { updatePlaybackTime(); },100);
+    
+}
+
+function DASH_MPD_loaded()
+{
+
+	console.log('MPD Loaded: entering function DASH_MPD_loaded()...');
+	myBandwidth = new bandwidth(bps, 1.1, 0.9);
+   
+	myMPD = dashInstance.mpdLoader.mpdparser.pmpd;
+	segNotAvailable = 0;
+	countSegmentRequest = 0;
+	
+    //extCount = 1;
+	var numS = Math.floor(checkDate(myMPD.availabilityStartTime) / myMPD.period[0].group[0].segmentTemplate.duration);
+	console.log("CheckDate(): "+checkDate(myMPD.availabilityStartTime));
+	console.log("Possibile numero segmento: "+numS);
+	extCount = numS;
+	console.log("extCount = "+extCount);
+	
+	adaptation = init_rateBasedAdaptation(dashInstance.mpdLoader.mpdparser.pmpd, dashInstance.videoTag, myBandwidth);
+	
+	/*
+   	myFplot = new fPlot(document.getElementById("graph").getContext("2d"),parsePT(dashInstance.mpdLoader.mpdparser.pmpd.mediaPresentationDuration),document.getElementById("graph").width,document.getElementById("graph").height);
+ 	myFplot.initNewFunction(0);
+	myFplot.initNewFunction(1);
+    	myFplot.initNewFunction(2); // the current playback time
+    	playbackTimePlot = myFplot;
+	myBandwidth.addObserver(myFplot);
+	
+	adaptation.addObserver(myFplot);*/
+	adaptation.switchRepresentation(); // try to get a better representation at the beginning
+	
+	//funzionante con 10,16 e 1000
+	console.log("minBufferTime: " + parsePT(myMPD.minBufferTime));
+	overlayBuffer = init_mediaSourceBuffer("0",0,parsePT(myMPD.minBufferTime),0,dashInstance.videoTag);
+	dashInstance.overlayBuffer = overlayBuffer;
+ 	
+	//qui avviene l'attach del mediaSource al tag video
+    /* new MSE ... */
+    var URL = window.URL || window.wekitURL;
+    if(window.WebKitMediaSource != null){
+        window.MediaSource = window.WebKitMediaSource;
+    }
+    var MSE = new window.MediaSource();
+    dashInstance.MSE = MSE;
+    dashInstance.videoTag.src = URL.createObjectURL(MSE);
+
+	
+	
+	console.log("MPD availabilityStartTime: "+myMPD.availabilityStartTime);
+	
+    dashInstance.MSE.addEventListener('webkitsourceopen', onOpenSource, false);
+	dashInstance.MSE.addEventListener('sourceopen', onOpenSource, false);
+
+	dashInstance.MSE.addEventListener('webkitsourceended', onSourceEnded);
+	dashInstance.MSE.addEventListener('sourceended', onOpenSource, false);
+	
+	overlayBuffer.addEventHandler(function(fillpercent, fillinsecs, max){ console.log("Event got called from overlay buffer, fillstate(%) = " + fillpercent + ", fillstate(s) = " + fillinsecs + ", max(s) = " + max); });
+    
+
+   	//window.setTimeout(function () { updatePlaybackTime(); },100);
+
+    
+}
+
+
+function DASHPlayer(videoTag, URLtoMPD)
+{
+	console.log("DASH-JS Version: " + DASHJS_VERSION);
+	dashInstance = this;
+	this.videoTag = videoTag;
+	initDASHttp('no-cache');
+	this.mpdLoader = new MPDLoader(DASH_MPD_loaded);
+	this.mpdLoader.loadMPD(URLtoMPD);
+	//myBuffer = init_timeBuffer(2,10,0,video);
+	//video.addEventListener('progress', , false);
+}
+
+
+/*
+* Ritorna la differenza in secondi tra l'ora corrente e l'inizio della trasmissione live (passata come parametro)
+*/
+function checkDate(startTime) {
+	
+	var parseDateI = startTime.split("T");
+	
+	var parseDate = parseDateI[0].split("-");
+	var parseTime = parseDateI[1].split(":");
+	
+	var startDate = new Date( parseDate[0], parseDate[1], parseDate[2], parseTime[0], parseTime[1], parseTime[2], null);
+	var now = new Date();
+	
+	var d1 = startDate.getTime();
+	var d2 = now.getTime();
+	
+	if (d1==d2) {
+		return 0;
+	} else if (d1>d2) {
+	
+		var diffS = now.getSeconds() - startDate.getSeconds();
+		var diffM = (now.getMinutes() - startDate.getMinutes())*60;
+		var diffH = (now.getHours() - startDate.getHours())*3600;
+		
+		return diffS + diffM + diffH;
+		
+	} else {
+		return -1;
+	}
+	
+}
